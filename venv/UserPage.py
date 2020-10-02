0bae69577bc2e3ae05bb8c65bd86fa8ba3f9e78e
Joung Jiwon <wjdwl001@khu.ac.kr>
2020-10-03 00:36:04 +0900

    User Save함수 구현

--- venv/UserPage.py
+++ venv/UserPage.py
@@ -25,7 +25,56 @@ class SampleApp(tk.Tk):
 
 
 class UserPage(tk.Frame):
+    #멤버변수
+    val_state = "" #00. 상태
+    val_ID = "" #0. ID(고유번호)
+    val_indexKorean = "" #1. 색인어(한글)
+    val_indexChinese = "" #2. 색인어(한자)
+    val_nickname = "" #3. 이명
+    val_generalName = "" #4. 범칭
+    val_majorClass = "" #9. 대분류
+    array_middleClass = [] #5. 중분류항목
+    array_subClass = [] #6. 소분류항목
+    val_relatedWord = "" #7. 관련어
+    val_detail = "" #8. 상세정보
+
     def __init__(self, master):
+        def save_temp(self):
+            if entry_state.get() == 1:
+                val_state = "대상"
+            if entry_state.get() == 2:
+                val_state = "비대상"
+            if entry_state.get() == 3:
+                val_state = "보류"
+            if entry_state.get() == 4:
+                val_state = "삭제"
+            val_ID  # 0. ID(고유번호)
+            val_indexKorean = indexKorean.get()  # 1. 색인어(한글)
+            val_indexChinese = indexChinese.get()  # 2. 색인어(한자)
+            val_nickname = nickname.get()  # 3. 이명
+            val_generalName = generalName.get()  # 4. 범칭
+            val_majorClass = majorClass.get()  # 9. 대분류
+            # 5. 중분류항목
+            if entry_middleClass_product.get():
+                array_middleClass.append("제작품")
+            if entry_middleClass_material.get():
+                array_middleClass.append("제작재료")
+            if entry_middleClass_tool.get():
+                array_middleClass.append("제작도구")
+            if entry_middleClass_producer.get():
+                array_middleClass.append("제작자")
+            # 6. 소분류항목
+            if entry_subClass_metal.get():
+                array_subClass.append("금속")
+            if entry_subClass_wood.get():
+                array_subClass.append("목재")
+            if entry_middleClass_tool.get():
+                array_middleClass.append("제작도구")
+            if entry_middleClass_producer.get():
+                array_middleClass.append("제작자")
+            val_relatedWord = relatedWord.get()  # 7. 관련어
+            val_detail = detail.get()  # 8. 상세정보
+
         tk.Frame.__init__(self, master)
 
         self.master = master
@@ -36,21 +85,10 @@ class UserPage(tk.Frame):
                  font=('맑은 고딕', 13)).pack()
         tk.Label(self, text="").pack()
 
+
         # 00. 상태
         frame00 = tk.Frame(self)
         frame00.pack(fill=tk.X)
-
-        def check():
-            global val_state
-            val_state = tk.StringVar()
-            if entry_state.get() == 1:
-                val_state = "대상"
-            if entry_state.get() == 2:
-                val_state = "비대상"
-            if entry_state.get() == 3:
-                val_state = "보류"
-            if entry_state.get() == 4:
-                val_state = "삭제"
 
         entry_state = tk.IntVar()
 
@@ -68,7 +106,6 @@ class UserPage(tk.Frame):
         entry_state_defer.pack(side=tk.RIGHT, padx=10)
         entry_state_nonsubject.pack(side=tk.RIGHT, padx=10)
         entry_state_subject.pack(side=tk.RIGHT, padx=10)
-        action = tk.Button(frame00, text="확인", command=check)
 
         # 구분선
         canvas = tkinter.Canvas(self, height=10, width=1000)
@@ -78,14 +115,13 @@ class UserPage(tk.Frame):
         # 저장
         frame = tk.Frame(self)
         frame.pack(side=tk.BOTTOM)
-        btnSave = tk.Button(frame, text="저장")
+        btnSave = tk.Button(frame, text="저장", command=save_temp)
         btnSave.pack(side=tk.LEFT, padx=10, pady=10)
 
         # 0. ID(고유번호)
         frame0 = tk.Frame(self)
         frame0.pack(fill=tk.X)
 
-        global val_ID
         val_ID = tk.StringVar()
         ID = uuid.uuid1()
         val_ID = ID.hex[0:9] #랜덤한 고유번호 16진수로 10자리로 만들었습니다
@@ -104,13 +140,12 @@ class UserPage(tk.Frame):
         frame1 = tk.Frame(self)
         frame1.pack(fill=tk.X)
 
-        global val_indexKorean
-        val_indexKorean = tk.StringVar()
+        indexKorean = tk.StringVar()
 
         lbl_indexKorean = tk.Label(frame1, text="색인어(한글)", width=10)
         lbl_indexKorean.pack(side=tk.LEFT, padx=10, pady=10)
 
-        entry_indexKorean = tk.Entry(frame1, textvariable=val_indexKorean)
+        entry_indexKorean = tk.Entry(frame1, textvariable=indexKorean)
         entry_indexKorean.pack(side=tk.LEFT, padx=10)
 
 
@@ -118,13 +153,12 @@ class UserPage(tk.Frame):
         frame2 = tk.Frame(self)
         frame2.pack(fill=tk.X)
 
-        global val_indexChinese
-        val_indexChinese = tk.StringVar()
+        indexChinese = tk.StringVar()
 
         lbl_indexChinese = tk.Label(frame2, text="색인어(한자)", width=10)
         lbl_indexChinese.pack(side=tk.LEFT, padx=10, pady=10)
 
-        entry_indexChinese = tk.Entry(frame2, textvariable=val_indexChinese)
+        entry_indexChinese = tk.Entry(frame2, textvariable=indexChinese)
         entry_indexChinese.pack(side=tk.LEFT,padx=10)
 
 
@@ -132,13 +166,12 @@ class UserPage(tk.Frame):
         frame3 = tk.Frame(self)
         frame3.pack(fill=tk.X)
 
-        global val_nickname
-        val_nickname = tk.StringVar()
+        nickname = tk.StringVar()
 
         lbl_nickname = tk.Label(frame3, text="이명", width=10)
         lbl_nickname.pack(side=tk.LEFT, padx=10, pady=10)
 
-        entry_nickname = tk.Entry(frame3, textvariable=val_nickname)
+        entry_nickname = tk.Entry(frame3, textvariable=nickname)
         entry_nickname.pack(side=tk.LEFT, padx=10)
 
 
@@ -146,13 +179,12 @@ class UserPage(tk.Frame):
         frame4 = tk.Frame(self)
         frame4.pack(fill=tk.X)
 
-        global val_generalName
-        val_generalName = tk.StringVar()
+        generalName = tk.StringVar()
 
         lbl_generalName = tk.Label(frame4, text="범칭", width=10)
         lbl_generalName.pack(side=tk.LEFT, padx=10, pady=10)
 
-        entry_generalName = tk.Entry(frame4, textvariable=val_generalName)
+        entry_generalName = tk.Entry(frame4, textvariable=generalName)
         entry_generalName.pack(side=tk.LEFT, padx=10)
 
 
@@ -161,32 +193,18 @@ class UserPage(tk.Frame):
         frame9.pack(fill=tk.X)
 
         values_detail = ["의궤", "실록", "승정원일기", "일성록", "전례서", "법전", "지리지", "등록", "발기", "유서류", "문집", "일기", "기타"]
-        global val_majorClass
-        val_majorClass = tk.StringVar()
+        majorClass = tk.StringVar()
 
         lbl_majorClass = tk.Label(frame9, text="대분류\n자료 유형별", width=10)
         lbl_majorClass.pack(side=tk.LEFT, padx=10, pady=10)
 
-        drBox_majorClass = tkinter.ttk.Combobox(frame9, height=15, values=values_detail, textvariable=val_majorClass ,state="readonly")
+        drBox_majorClass = tkinter.ttk.Combobox(frame9, height=15, values=values_detail, textvariable=majorClass ,state="readonly")
         drBox_majorClass.pack(side=tk.LEFT, pady=10, padx=10, expand=False)
 
 
         # 5. 중분류항목
         frame5 = tk.Frame(self)
         frame5.pack(fill=tk.X)
-
-        array_middleClass = []
-
-        #이 함수는 나중에 저장할 때 실행할게요
-        def middleClass_array():
-            if entry_middleClass_product.get():
-                array_middleClass.append("제작품")
-            if entry_middleClass_material.get():
-                array_middleClass.append("제작재료")
-            if entry_middleClass_tool.get():
-                array_middleClass.append("제작도구")
-            if entry_middleClass_producer.get():
-                array_middleClass.append("제작자")
 
         lbl_middleClass = tk.Label(frame5, text="중분류항목", width=10)
         lbl_middleClass.pack(side=tk.LEFT, padx=10, pady=10)
@@ -215,18 +233,6 @@ class UserPage(tk.Frame):
 
         lbl_subClass = tk.Label(frame6, text="소분류항목", width=10)
         lbl_subClass.pack(side=tk.LEFT, padx=10, pady=10)
-
-        array_subClass = []
-
-        def subClass_array():
-            if entry_subClass_metal.get():
-                array_subClass.append("금속")
-            if entry_subClass_wood.get():
-                array_subClass.append("목재")
-            if entry_middleClass_tool.get():
-                array_middleClass.append("제작도구")
-            if entry_middleClass_producer.get():
-                array_middleClass.append("제작자")
 
         ckBox_subClass_metal = tk.IntVar()
         ckBox_subClass_wood = tk.IntVar()
@@ -269,13 +275,12 @@ class UserPage(tk.Frame):
         frame7 = tk.Frame(self)
         frame7.pack(fill=tk.X)
 
-        global val_relatedWord
-        val_relatedWord = tk.StringVar()
+        relatedWord = tk.StringVar()
 
         lbl_relatedWord = tk.Label(frame7, text="관련어", width=10)
         lbl_relatedWord.pack(side=tk.LEFT, padx=10, pady=10)
 
-        entry_relatedWord = tk.Entry(frame7)
+        entry_relatedWord = tk.Entry(frame7, textvariable=relatedWord)
         entry_relatedWord.pack(side=tk.LEFT, padx=10)
 
 
@@ -283,14 +288,14 @@ class UserPage(tk.Frame):
         frame8 = tk.Frame(self)
         frame8.pack(fill=tk.X)
 
-        global val_detail
-        val_detail = tk.StringVar()
+        detail = tk.StringVar()
 
         lbl_detail = tk.Label(frame8, text="상세정보", width=10)
         lbl_detail.pack(side=tk.LEFT, padx=10, pady=10)
 
         entry_detail = tk.Text(frame8)
         entry_detail.pack(fill=tk.X, padx=10, expand=True)
+
 
 
 
